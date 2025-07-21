031352a8e1372445f71bef853d7385b864c06f53
Михаил Васильевич Прикоп <prikop_mv@corp.istp.ru>
2025-07-21 12:15:47 +0100

    Add game logic

--- functions.py
+++ functions.py
@@ -1,40 +1,72 @@
 import random
 
 #функция  выбора хода компьютера
-def choice_computer(primary_choice = ['rock', 'paper', 'scissors']):
+def choice_computer(primary_choice = ['rock', 'paper', 'scissors'], language = 'EN'):
+    if language == 'RU':
+        primary_choice = ['камень', 'бумага', 'ножницы']
     return random.choice(primary_choice)
 
 #функция выбора пользователя
-def choice_user(input_user):
-    if input_user.lower == 'КАМЕНЬ' or input_user.lower == 'ROCK' or input_user == '1':
-        return 'rock'
-    elif input_user.lower == 'БУМАГА' or input_user.lower == 'PAPER' or input_user == '2':
-        return 'paper'
-    else:
-        return 'scissors'
+def choice_user(input_user, language = 'EN'):
+    if language == 'RU':
+        if input_user.upper == 'КАМЕНЬ' or input_user == '1':
+            return 'камень'
+        elif input_user.upper == 'БУМАГА' or input_user == '2':
+            return 'бумага'
+        else:
+            return 'ножницы'
+    else :
+        if input_user.upper == 'ROCK' or input_user == '1':
+            return 'rock'
+        elif input_user.upper == 'PAPER' or input_user == '2':
+            return 'paper'
+        else:
+            return 'scissors'
 
 #функция определения победителя
-def winner(user, computer,):
-    if (
-            (user == 'rock' and computer == 'scissors')
-            or
-            (user == 'paper' and computer == 'rock' )
-            or
-            user == 'scissors' and computer == 'paper'
-    ):
-        return 'wine'
+def winner(user, computer, language = 'EN'):
+    if language == 'RU':
+        if (
+                (user == 'камень' and computer == 'ножницы')
+                or
+                (user == 'бумага' and computer == 'камень')
+                or
+                user == 'ножницы' and computer == 'бумага'
+        ):
+            return 'ВЫ ПОБЕДИЛИ :)'
 
-    elif (
-            (user == 'rock' and computer == 'rock')
-            or
-            (user == 'paper' and computer == 'paper')
-            or
-            (user == 'scissors' and computer == 'scissors')
-    ):
-        return 'draw'
+        elif (
+                (user == 'камень' and computer == 'КАМЕНЬ')
+                or
+                (user == 'бумага' and computer == 'бумага')
+                or
+                (user == 'ножницы' and computer == 'НОЖНИЦЫ')
+        ):
+            return 'НИЧЬЯ :|'
 
+        else:
+            return 'ВЫ ПРОИГРАЛИ :('
     else:
-        return 'loss'
+        if (
+                (user == 'rock' and computer == 'scissors')
+                or
+                (user == 'paper' and computer == 'rock' )
+                or
+                user == 'scissors' and computer == 'paper'
+        ):
+            return 'WINE :)'
+
+        elif (
+                (user == 'rock' and computer == 'rock')
+                or
+                (user == 'paper' and computer == 'paper')
+                or
+                (user == 'scissors' and computer == 'scissors')
+        ):
+            return 'DRAW :|'
+
+        else:
+            return 'LOSS :('
 
 
 
